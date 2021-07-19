ff81b30287bf62154f8c61a9cb116247aed24f04
Nelson Aquino Jr <naquinojr@outlook.com>
2021-07-19 08:01:21 -0700

    Initial Commit

---
+++ Bopomofo Studio/Bopomofo_StudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bopomofo_StudioApp.swift
+//  Bopomofo Studio
+//
+//  Created by Nelson Aquino Jr  on 7/19/21.
+//
+
+import SwiftUI
+
+@main
+struct Bopomofo_StudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
