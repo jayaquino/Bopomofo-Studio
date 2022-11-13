73e89948355d3a49c457b9e229135b66fd00fc1e
Nelson Aquino Jr <naquino@rapptrlabs.com>
2022-11-13 18:21:21 +0800

    [Clean] Folder cleanup

--- MockProvider/Sources/MockProvider/MockContentProvider.swift
+++ MockProvider/Sources/MockProvider/MockContentProvider.swift
@@ -11,6 +11,23 @@ public class MockContentProvider: ContentProvider {
     }
     
     public func retrieveScores(testType: ContentStore.TestType, scoreModel: ScoreModel) async throws -> [ScoreModel] {
-        throw URLError(.badURL)
+        [
+            ScoreModel(
+                score: 12,
+                time: 30.0
+            ),
+            ScoreModel(
+                score: 15,
+                time: 30
+            ),
+            ScoreModel(
+                score: 25,
+                time: 30.0
+            ),
+            ScoreModel(
+                score: 30,
+                time: 30
+            )
+        ]
     }
 }
